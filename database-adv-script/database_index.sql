8f2fae58fdc65e7a6d44f11d43a05c2d5b9e2324
Damaris Chege <deenyashke@gmail.com>
2025-05-17 14:45:14 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -7,3 +7,8 @@ CREATE INDEX idx_booking_property_id ON Booking(property_id);
 -- Create index on User.email
 CREATE INDEX idx_user_email ON User(email);
 
+-- Measure performance using EXPLAIN ANALYZE
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 1;
+
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE property_id = 1;
+

780c8187e27c25d8d4d5887bd2e24ec5c43acfc4
Damaris Chege <deenyashke@gmail.com>
2025-05-17 14:51:45 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,1 +1,18 @@
+-- Original Query: Get all booking info with user, property, and payment details
+SELECT 
+    b.id AS booking_id,
+    u.name AS user_name,
+    u.email,
+    p.name AS property_name,
+    p.location,
+    pay.amount,
+    pay.status
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.id
+JOIN 
+    properties p ON b.property_id = p.id
+JOIN 
+    payments pay ON b.id = pay.booking_id;
 
